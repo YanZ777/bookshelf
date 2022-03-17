c4087c26eb9cdfac3aca388ecbca166fa69f3087
Yan <yanzhao777@gmail.com>
2022-03-17 01:24:28 -0700

    Complete exercise 1, Extra Credit 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,46 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import {Logo} from './components/logo'
+import Dialog from '@reach/dialog';
+import "@reach/dialog/styles.css";
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
+function App() {
+   const [showLoginDialog, setShowLoginDialog] = React.useState(false);
+   const [showRegisterDialog, setShowRegisterDialog] = React.useState(false);
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+   const onOpenLoginDialog = ()=> {
+      setShowLoginDialog(true);
+   };
+   const onOpenRegisterDialog = () => {
+      setShowRegisterDialog(true);
+   };
+
+   const onCloseLoginDialog = () => {
+      setShowLoginDialog(false);
+   };
+   const onCloseRegisterDialog = () => {
+      setShowRegisterDialog(false);
+   };
+
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+         <div>
+         <button onClick={onOpenLoginDialog}>Login</button>
+         </div>
+      <Dialog isOpen={showLoginDialog} onDismiss={onCloseLoginDialog}>
+         Login
+      </Dialog>
+         <div>
+         <button onClick={onOpenRegisterDialog}>Register</button>
+         </div>
+      <Dialog isOpen={showRegisterDialog} onDismiss={onCloseRegisterDialog}>
+         Register
+      </Dialog>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
