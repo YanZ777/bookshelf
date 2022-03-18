a6fb48bd195ce8bfc67b1fd2757eb5956650f093
Yan <yanzhao777@gmail.com>
2022-03-18 00:40:45 -0700

    Complete Exercise 1, Extra Credit 2

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,43 +1,52 @@
+import '@reach/dialog/styles.css'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
-import Dialog from '@reach/dialog';
-import "@reach/dialog/styles.css";
 
+export default function LoginForm({onSubmit}) {
+   return (
+   <form>
+      <div>
+         <input type="text" text="username"></input>
+      </div>
+      <div>
+         <input type="password" text="password"></input>
+      </div>
+      <button onClick={onSubmit}>Submit</button>
+   </form>);
+ }
 
 function App() {
-   const [showLoginDialog, setShowLoginDialog] = React.useState(false);
-   const [showRegisterDialog, setShowRegisterDialog] = React.useState(false);
+  const [openModal, setOpenModal] = React.useState('none')
 
-   const onOpenLoginDialog = ()=> {
-      setShowLoginDialog(true);
-   };
-   const onOpenRegisterDialog = () => {
-      setShowRegisterDialog(true);
-   };
-
-   const onCloseLoginDialog = () => {
-      setShowLoginDialog(false);
-   };
-   const onCloseRegisterDialog = () => {
-      setShowRegisterDialog(false);
-   };
+  function handleSubmit(formData) {
+   console.log('login', formData)
+ }
 
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-         <div>
-         <button onClick={onOpenLoginDialog}>Login</button>
-         </div>
-      <Dialog isOpen={showLoginDialog} onDismiss={onCloseLoginDialog}>
-         Login
+      <div>
+        <button onClick={() => setOpenModal('login')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => setOpenModal('register')}>Register</button>
+      </div>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+        <LoginForm onSubmit={handleSubmit} buttonText="Login" />
       </Dialog>
-         <div>
-         <button onClick={onOpenRegisterDialog}>Register</button>
-         </div>
-      <Dialog isOpen={showRegisterDialog} onDismiss={onCloseRegisterDialog}>
-         Register
+      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+        <LoginForm onSubmit={handleSubmit} buttonText="Login" />
       </Dialog>
     </div>
   )
