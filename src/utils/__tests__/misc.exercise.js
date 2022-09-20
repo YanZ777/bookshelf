759feabd4f77159d24c5df8c64377d226adc185e
Yan <yanzhao777@gmail.com>
2022-09-20 00:32:57 -0700

    Complete Exercise 11

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,6 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+// test.todo('formatDate formats the date to look nice')
+test('formatData formats the date to look nice', () => {
+  expect(formatDate(new Date('September 20, 2022'))).toBe('Sep 22')
+})
